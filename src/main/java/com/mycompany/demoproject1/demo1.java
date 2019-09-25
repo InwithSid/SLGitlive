d2554a8ff334a16ac432402d55ca0f85a180eb4b
SidLabs <sidhartha.mitra@live.com>
2019-09-25 12:26:58 +0530

    Initial Commit.

---
+++ src/main/java/com/mycompany/demoproject1/demo1.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.demoproject1;
+
+/**
+ *
+ * @author Administrator
+ */
+public class demo1 {
+    public static void main(String[]args){
+        
+        System.out.println("What a wonderful world!");
+        
+        int worldPopulation = 300+12;
+        
+        System.out.println("Initial inhibitants : " + worldPopulation);
+    }
+    
+}
